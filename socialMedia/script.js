ba73d9b97c5bd9b17f0aa0d2f18d6466e81d967a
Pavlo Shchur <pavloshchur@gmail.com>
2018-03-12 08:22:02 +0200

    JavaScript : socialMedia directory: in script.js for loop is optimized.

--- socialMedia/script.js
+++ socialMedia/script.js
@@ -1,22 +1,21 @@
-var socialMedia = {
+let socialMedia = {
   facebook : 'http://facebook.com/viewsource',
   twitter: 'http://twitter.com/planetoftheweb',
   flickr: 'http://flickr.com/planetotheweb',
   youtube: 'http://youtube.com/planetoftheweb'
 };
 
-var socialList = function() {
-  var  output = '<ul>', 
+let socialList = function() {
+  let  output = '<ul>',
     myList = document.querySelectorAll('.socialmediaicons');
 
-  for (var key in arguments[0]) {
+  for (let key in arguments[0]) {
     output+= '<li><a href="' + socialMedia[key] + '">' +
-      '<img src="images/' + key + '.png" alt="icon for '+key+'">' +
-      '</a></li>';
+      '<img src="images/' + key + '.png" alt="icon for ' + key + '"></a></li>';
   }
   output+= '</ul>';
   
-  for (var i = myList.length - 1; i >= 0; i--) {
+  for (let i = 0, x = myList.length; i < x; i++) {
     myList[i].innerHTML = output;
   };
 }(socialMedia);
